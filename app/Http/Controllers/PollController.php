19c9cef245d87bf8f5999bf55dd93b7c76fd0007
BayuDC <dev.bayudc@gmail.com>
2022-03-12 21:12:13 +0700

    Save poll data to database

--- app/Http/Controllers/PollController.php
+++ app/Http/Controllers/PollController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Choice;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Poll;
 
 class PollController extends Controller {
     public function index() {
@@ -12,13 +15,38 @@ class PollController extends Controller {
         return view('poll.create');
     }
     public function store(Request $request) {
-        $validated = $request = $request->validate([
+        $request->validate([
             'title' => 'required',
             'description' => 'required',
             'deadline' => 'required|date',
             'choice1' => 'required',
             'choice2' => 'required',
-            'choice3' => 'required',
         ]);
+
+        $poll = new Poll;
+        $poll->title = $request->title;
+        $poll->description = $request->description;
+        $poll->deadline = $request->deadline;
+        $poll->created_by = Auth::user()->id;
+        $poll->save();
+
+        $choice = new Choice;
+        $choice->choice = $request->choice1;
+        $choice->poll_id = $poll->id;
+        $choice->save();
+
+        $choice = new Choice;
+        $choice->choice = $request->choice2;
+        $choice->poll_id = $poll->id;
+        $choice->save();
+
+        if ($request->choice3) {
+            $choice = new Choice;
+            $choice->choice = $request->choice3;
+            $choice->poll_id = $poll->id;
+            $choice->save();
+        }
+
+        return redirect('/poll');
     }
 }
