8427aae20c02d8afa340c331b9f1d43c1eb0e59e
BayuDC <dev.bayudc@gmail.com>
2022-03-12 11:07:23 +0700

    Add database models and migrations

--- app/Models/Poll.php
+++ app/Models/Poll.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model {
+class Poll extends Model
+{
     use HasFactory;
 }
---
+++ database/migrations/2022_03_12_034854_relationships.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreign('division_id')->references('id')->on('divisions');
+        });
+        Schema::table('polls', function (Blueprint $table) {
+            $table->foreign('created_by')->references('id')->on('users');
+        });
+        Schema::table('choices', function (Blueprint $table) {
+            $table->foreign('poll_id')->references('id')->on('polls');
+        });
+        Schema::table('votes', function (Blueprint $table) {
+            $table->foreign('choice_id')->references('id')->on('choices');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('poll_id')->references('id')->on('polls');
+            $table->foreign('division_id')->references('id')->on('divisions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('division_id');
+        });
+        Schema::table('polls', function (Blueprint $table) {
+            $table->dropForeign('created_by');
+        });
+        Schema::table('choices', function (Blueprint $table) {
+            $table->dropForeign('poll_id');
+        });
+        Schema::table('votes', function (Blueprint $table) {
+            $table->dropForeign('choice_id');
+            $table->dropForeign('user_id');
+            $table->dropForeign('poll_id');
+            $table->dropForeign('division_id');
+        });
+    }
+};
